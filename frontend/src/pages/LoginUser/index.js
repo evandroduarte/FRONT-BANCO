5f81e1021bebe10c5841994cb8332454a795f65b
Evandro Duarte <duarte.evandro@gmail.com>
2020-08-04 13:08:50 -0300

    Instalaçao do axios e ligaçao com back usuarios

--- frontend/src/pages/LoginUser/index.js
+++ frontend/src/pages/LoginUser/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
-
+import api from '../../services/api';
 
 /*import './styles.css';*/
 import {Fundo, Content} from './styles'
@@ -9,14 +9,47 @@ import {Fundo, Content} from './styles'
 import foruser from '../../assets/forhelpers.png';
 
 function LoginONG(){
+    
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const history = useHistory();
+
+    async function handleLogin(e){
+        e.preventDefault();
+
+        try{
+            const response = await api.post('users/session', {email, password});
+
+            console.log(response);
+
+            localStorage.setItem('userName', response.data.name);
+
+            history.push('/');
+        }catch(err){
+            alert('Falha no login');
+        }
+    }
+
     return (
         <Fundo>
             <Content>
                 
                 <img src={foruser} alt="bannner" className="banner"/>
-                    <form>
-                        <input placeholder="E-mail" />
-                        <input placeholder="Senha" />
+                    <form onSubmit={handleLogin}>
+                        <input 
+                        placeholder="E-mail"
+                        required 
+                        value= {email}
+                        onChange={e => setEmail(e.target.value)}
+                        />
+                        <input 
+                        placeholder="Senha" 
+                        required
+                        type="password"
+                        value= {password}
+                        onChange={e => setPassword(e.target.value)}
+                        />
                         <button type="submit">Entrar</button>
                         <div className="cadastro">
                             <Link to="/register" className="btn">
--- frontend/src/pages/RegisterUser/index.js
+++ frontend/src/pages/RegisterUser/index.js
@@ -1,10 +1,45 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import api from '../../services/api';
 
 
 import logo from '../../assets/logo_semfundo.png';
 
 function RegisterUser(){
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [whatsapp, setWhatsapp] = useState('');
+    const [city, setCity] = useState('');
+    const [uf, setUf] = useState('');
+
+    const history = useHistory();
+
+
+    async function handleRegister(e){
+        const data = {
+            name,
+            email,
+            password,
+            whatsapp,
+            city,
+            uf 
+        }
+
+        try{
+            const response = await api.post('users', data);
+
+            alert(`Cadastro realizado com sucesso! ${name}`);
+            history.push('/loginuser');
+        }catch(err){
+            alert(`Erro no cadastro, tente novamente`);
+        }
+
+
+        e.preventDefault();
+    }
+
+
     return (
         <div className="register-container">
             <div className="content">
@@ -16,16 +51,40 @@ function RegisterUser(){
                 <p>    - Ajude os animais perdidos a encontrarem seus donos </p>
                 </section>
 
-                <form className="form">
+                <form className="form" onSubmit={handleRegister}>
                     <h1>Preencha com os dados da ONG</h1>
-                    <input placeholder="Nome completo" required/>
-                    <input placeholder="Telefone (Whatsapp)" required/>
+                    <input 
+                    placeholder="Nome completo" required
+                    value={name}
+                    onChange={ e => setName(e.target.value)}
+                    />
+                    <input 
+                    placeholder="Telefone (Whatsapp)" required
+                    value={whatsapp}
+                    onChange={ e => setWhatsapp(e.target.value)}
+                    />
                     <div className="input-group">
-                    <input placeholder="Cidade"/>
-                    <input placeholder="UF" required maxLength="2" style={{ width: 80 }} />
+                    <input 
+                    placeholder="Cidade"
+                    value={city}
+                    onChange={ e => setCity(e.target.value)}
+                    />
+                    <input 
+                    placeholder="UF" required maxLength="2" style={{ width: 80 }}
+                    value={uf}
+                    onChange={ e => setUf(e.target.value)}
+                    />
                     </div>
-                    <input placeholder="E-mail" required type="email" />
-                    <input placeholder="Senha" required type="password"/>
+                    <input 
+                    placeholder="E-mail" required type="email" 
+                    value={email}
+                    onChange={ e => setEmail(e.target.value)}
+                    />
+                    <input 
+                    placeholder="Senha" required type="password"
+                    value={password}
+                    onChange={ e => setPassword(e.target.value)}
+                    />
                     <button className="button" type="submit">Cadastrar</button>
                 </form>
                 </div>
