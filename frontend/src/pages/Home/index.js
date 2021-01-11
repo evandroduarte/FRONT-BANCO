ac519c97761662767cb4bc74171375709b0bdf78
Luiz G <luiz13lg13@gmail.com>
2021-01-11 15:50:06 -0300

    Alterações na home

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,159 +1,243 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-/*import './styles.css';*/
-import {Fundo, Content, Banner, QmSomos, CausasTitulo, Listagem, CardONG, CardAnimal, CasosAnimal, CasosONG} from './styles';
+import './styles.css';
+
+import {
+  Fundo,
+  Content,
+  Banner,
+  QmSomos,
+  CausasTitulo,
+  Listagem,
+  CardONG,
+  CardAnimal,
+  CasosAnimal,
+  CasosONG,
+} from "./styles";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
 /* Simple Bar import */
-import SimpleBar from 'simplebar-react';
-import 'simplebar/dist/simplebar.min.css';
+import SimpleBar from "simplebar-react";
+import "simplebar/dist/simplebar.min.css";
 
-import { Link, useHistory } from 'react-router-dom';
-import { FiMapPin } from 'react-icons/fi';
+import { Link, useHistory } from "react-router-dom";
+import { FiMapPin } from "react-icons/fi";
 
-import banner from '../../assets/banner.png';
-import quemSomos from '../../assets/quemSomos.png';
-import aboutBanner from '../../assets/aboutBanner.png';
-import causasOngs from '../../assets/causasongs.png';
-import animaisPerdidos from '../../assets/animaisperdidos.png';
-import causasTitulo from '../../assets/causastitulo.png';
-import api from '../../services/api';
-import { useEffect } from 'react';
+import banner from "../../assets/banner.png";
+import quemSomos from "../../assets/quemSomos.png";
+import aboutBanner from "../../assets/aboutBanner.png";
+import causasOngs from "../../assets/causasongs.png";
+import animaisPerdidos from "../../assets/animaisperdidos.png";
+import causasTitulo from "../../assets/causastitulo.png";
+import api from "../../services/api";
+import { useEffect } from "react";
 
+function Login() {
+  const [incidents, setIncidents] = useState([]);
+  const [lostanimals, setLostAnimals] = useState([]);
 
-function Login(){
+  const history = useHistory();
 
-        const [incidents, setIncidents] = useState([]);
-        const [lostanimals, setLostAnimals] = useState([]);
+  useEffect(() => {
+    api.get("lostanimals").then((response) => {
+      setLostAnimals(response.data);
+      console.log(response.data);
+    });
+  }, []);
 
-        const history = useHistory();
-        
-        useEffect(() => {
-            api.get('lostanimals').then(response => {
-                setLostAnimals(response.data);
-            })
-        }, []);
+  useEffect(() => {
+    api.get("donationrequest").then((response) => {
+      setIncidents(response.data);
+    });
+  }, []);
 
+  function handleLogout() {
+    localStorage.clear();
 
-        useEffect(() => {
-            api.get('donationrequest').then(response => {
-                setIncidents(response.data);
-            })
-        }, []);
+    history.push("/");
+  }
 
-        function handleLogout() {
-            localStorage.clear();
-        
-            history.push('/');
-          }
+  function formatDate(input) {
+    var datePart = input.match(/\d+/g),
+      year = datePart[0].substring(2), // get only two digits
+      month = datePart[1],
+      day = datePart[2];
 
+    return day + "/" + month + "/" + year;
+  }
 
-        function formatDate (input) {
-            var datePart = input.match(/\d+/g),
-            year = datePart[0].substring(2), // get only two digits
-            month = datePart[1], day = datePart[2];
-          
-            return day+'/'+month+'/'+year;
-          }
+  return (
+    <Fundo>
+      <Content>
+        <Banner>
+          <img src={banner} alt="bannner" className="banner" />
+        </Banner>
+        <div className="bemVindo">
+          <h1>Bem vindo ao Look For!</h1>
+          <p className="subTitulo">
+            A plataforma que nos une em prol do bem estar animal
+          </p>
+        </div>
+        <div className="btnBemVindo">
+          <button type="submit" className="buttonONG">
+            <Link
+              to="/loginong"
+              style={{ textDecoration: "none", color: "white" }}
+            >
+              ONG
+            </Link>
+          </button>
 
-    return (
-        <Fundo>
-            <Content>
-                <Banner>
-                    <img src={banner} alt="bannner" className="banner"/>
-                </Banner>
-                <div className="bemVindo">
-                    <h1>Bem vindo ao Look For!</h1>
-                    <p className="subTitulo">A plataforma que nos une em prol do bem estar animal</p>
-                    
-                </div>
-                <div className="btnBemVindo">
-                    
-                    <button type="submit" >
-                        <Link to="/loginong" style={{ textDecoration: 'none', color: 'white' }}>
-                            ONG
-                        </Link>
-                    </button>
-                        
-                    
-                    <button type="submit" className="buttonUser">
-                        <Link to="/loginuser" style={{ textDecoration: 'none', color: 'white' }}>
-                            Colaborador
-                        </Link>
-                    </button>
-                </div>
-                <QmSomos>
-                    <div className="aboutUs">
-                        <img src={quemSomos} alt="quemSomos"/>
+          <button type="submit" className="buttonUser">
+            <Link
+              to="/loginuser"
+              style={{ textDecoration: "none", color: "white" }}
+            >
+              Colaborador
+            </Link>
+          </button>
+        </div>
+        <QmSomos>
+          <div className="aboutUs">
+            <img src={quemSomos} alt="quemSomos" />
+          </div>
+          <div className="aboutUsContent">
+            <p style={{ textAlign: "right" }}>
+              A <b>Look For</b> é uma organização não lucrativa com o objetivo
+              de unir causas animais. Nosso objetivo é atingido disponibilizando
+              uma plataforma totalmente online e gratuita para Ongs informarem
+              suas causas e necessidades de doações e para usuários cadastrarem
+              animais perdidos ou consultar dados de Ongs.
+              <br />
+              <br />
+              Participe com doações ou cadastre sua Ong!
+              <br />
+              <b>Venha fazer parte!</b>
+            </p>
+            <img src={aboutBanner} alt="aboutBanner" />
+          </div>
+        </QmSomos>
+
+        <CausasTitulo>
+          <img src={causasTitulo} alt="Titulo" />
+        </CausasTitulo>
+
+        <Listagem>
+          <CasosAnimal>
+            <img
+              src={animaisPerdidos}
+              alt="Animais Perdidos"
+              className="titulos"
+            />
+            <SimpleBar style={{ height: "1300px" }}>
+              <ul className="list">
+                {lostanimals.map((lostanimal) => (
+                  <>
+                    {/*Modal Animal Perdido */}
+                    <div className="modal-animal-perdido" id={`LostAnimalModal-${lostanimal.LA_id}`}>
+                      <div className="modal-animal-perdido-main">
+                        <div className="header-modal-animal-editar-doacao">
+                          <h2 className="titulo-animal-perdido">Informações Detalhadas</h2>
+                          <div className="botao-fechar" onClick={() => fecharModal(lostanimal.LA_id)}>
+                            <FontAwesomeIcon icon={faTimes} />
+                          </div>
+                        </div>
+                        <div className="div-imagem">
+                          <img className="LA_modal-imagem" src={lostanimal.LA_image.replace("uploads","http://localhost:3333")} alt="Foto do Animal"/>
+                        </div>
+                        <div className="informacoes-modal-animal-perdido">
+                          <p>{lostanimal.LA_description}</p>
+                        </div>
+                      </div>
                     </div>
-                    <div className="aboutUsContent">
-                        <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                        </p>
-                        <img src={aboutBanner} alt="aboutBanner"/>
-                    </div>
-                    
-                </QmSomos>
 
-                <CausasTitulo>
-                    <img src={causasTitulo} alt="Titulo"/>
-                </CausasTitulo>
+                    <li key={lostanimal.LA_id}>
+                      <CardAnimal onClick={() => {abrirModal(lostanimal.LA_id)}}>
+                        <div className="media">
+                          <img
+                            src={lostanimal.LA_image.replace(
+                              "uploads",
+                              "http://localhost:3333"
+                            )}
+                            className="imagem"
+                            alt="Lost Animal"
+                          />
+                        </div>
+                        <div className="descricao">
+                          <p>{lostanimal.LA_description}</p>
+                          <h3>{formatDate(lostanimal.LA_date)}</h3>
+                          <div className="localizacao">
+                            <FiMapPin size={16} color="#463B88" />
+                            <h4>Localização</h4>
+                          </div>
+                        </div>
+                      </CardAnimal>
+                    </li>
+                  </>
+                ))}
+              </ul>
+            </SimpleBar>
+          </CasosAnimal>
+          <CasosONG>
+            <img src={causasOngs} alt="Causas Ongs" className="titulos" />
+            <SimpleBar style={{ height: "1300px" }}>
+              <ul>
+                {incidents.map((incident) => (
+                  <li key={incident.DR_id}>
+                    <CardONG>
+                      <div className="media">
+                        <img
+                          src={incident.ong_image.replace(
+                            "uploads",
+                            "http://localhost:3333"
+                          )}
+                          className="imagem"
+                          alt="Ong"
+                        />
+                      </div>
+                      <div className="descricao">
+                        <h1>{incident.ong_name}</h1>
+                        <p>{incident.DR_description}</p>
+                        <h4>{formatDate(incident.DR_date)}</h4>
+                        <div className="localizacao">
+                          <h3>
+                            {Intl.NumberFormat("pt-BR", {
+                              style: "currency",
+                              currency: "BRL",
+                            }).format(incident.DR_money)}
+                          </h3>
+                        </div>
+                      </div>
+                    </CardONG>
+                  </li>
+                ))}
+              </ul>
+            </SimpleBar>
+          </CasosONG>
+        </Listagem>
+      </Content>
+    </Fundo>
+  );
 
-                <Listagem>
-                    <CasosAnimal>
-                        <img src={animaisPerdidos} alt="Animais Perdidos" className="titulos" />
-                     <SimpleBar style={{ height: '1300px' }}>
-                        <ul className="list">
-                            {lostanimals.map(lostanimal => (
-                                <li key = {lostanimal.LA_id}>
-                                <CardAnimal>
-                                        <div className="media">
-                                        <img src={(lostanimal.LA_image).replace('uploads', 'http://localhost:3333')} className="imagem" alt="Lost Animal"/>
-                                        </div>
-                                <div className="descricao">
-                                <p>{lostanimal.LA_description}</p>
-                                <h3>{formatDate(lostanimal.LA_date)}</h3>
-                                <div className="localizacao">
-                                    <FiMapPin size={16} color="#463B88" /> 
-                                    <h4>Localização</h4>
-                                </div>
-                            </div>
-                        </CardAnimal>
-                                </li>
-                            ))}
-                        </ul>
-                    </SimpleBar>
-                    </CasosAnimal>
-                    <CasosONG>
-                        <img src={causasOngs} alt="Causas Ongs" className="titulos" />
-                        <SimpleBar style={{ height: '1300px' }}>
-                            <ul>
-                            {incidents.map(incident => (
-                                <li key = {incident.DR_id}>
-                                <CardONG>
-                                    <div className="media">
-                                <img src = {(incident.ong_image).replace('uploads', 'http://localhost:3333')} className="imagem" alt= "Ong"/>
-                                    </div>
-                                <div className="descricao">
-                                <h1>{incident.ong_name}</h1>
-                                <p>{incident.DR_description}</p>
-                                <h4>{formatDate(incident.DR_date)}</h4>
-                                <div className="localizacao">
-                                    <h3>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.DR_money)}</h3>
-                                </div>
-                            </div>
-                        </CardONG>
-                                </li>
-                            ))}
-                        </ul>
-                        </SimpleBar>
-                    </CasosONG>
-                </Listagem>
-            </Content>
-            
-        </Fundo>
-        
-    );
-};
+  /**
+   * Fecha o modal aberto.
+   * @param {Number} index Indice do modal.
+   */
+  function fecharModal(index) {
+    document
+      .querySelector(`#LostAnimalModal-${index}`)
+      .setAttribute("style", "display:none");
+  }
+
+  function abrirModal(index) {
+    //tornando o modal visivel
+    
+    document
+      .querySelector(`#LostAnimalModal-${index}`)
+      .setAttribute("style", "display:block");
+  }
+}
 
 export default Login;
--- frontend/src/pages/Home/styles/index.js
+++ frontend/src/pages/Home/styles/index.js
@@ -1,234 +1,232 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Fundo = styled.div`
-    width: 100%;
-    
-    margin: 0 auto auto auto;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    
-    color: #463B88;
+  width: 100%;
+
+  margin: 0 auto auto auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  color: #463b88;
 `;
 
 export const Content = styled.div`
-    width: 70%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+  width: 70%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 
-    .bemVindo{
-        width: 85%;
-    }
-    .btnBemVindo{
-        display: flex;
-        align-items: center;
-        width: 73%;
-        justify-content: space-between;
-    }
-    button{
-        width: 40%;
-        height: 50px;
-        margin-top: 10px;
-        background: #453C8A;
-        border-radius: 10px;
-        color: #FFFFFF;
-    }
-    
-    .subTitulo{
-        font-size: 18px;
-    }
+  .bemVindo {
+    width: 85%;
+  }
+  .btnBemVindo {
+    display: flex;
+    align-items: center;
+    width: 73%;
+    justify-content: space-between;
+  }
+  button {
+    width: 40%;
+    height: 50px;
+    margin-top: 10px;
+    background: #453c8a;
+    border-radius: 10px;
+    color: #ffffff;
+    border-style: none;
+    font-weight: bold;
+  }
+
+  button:hover {
+    background: #554f82;
+  }
+
+  .subTitulo {
+    font-size: 18px;
+  }
 `;
 
 export const Banner = styled.div`
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    img{
-        width: 73%;
-    }
-    
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  img {
+    width: 73%;
+  }
 `;
 
 export const QmSomos = styled.div`
-    width: 85%;
-    margin-top: 10px;
-    display: flex;
-    flex-direction: column;
-    .aboutUs{
-        img{
-            width: 40%;
-        }
+  width: 85%;
+  margin-top: 10px;
+  display: flex;
+  flex-direction: column;
+  .aboutUs {
+    img {
+      width: 40%;
     }
-    
+  }
 
-    .aboutUsContent{
-        width: 100%;
-        display: flex;
-        justify-content: space-between;
-        
-        p{
-            text-align: justify;
-            font-weight: normal;
-            font-size: 18px;
-        }
-        img{
-            width: 50%;
-        }
+  .aboutUsContent {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+
+    p {
+      text-align: justify;
+      font-weight: normal;
+      font-size: 18px;
     }
+    img {
+      width: 50%;
+    }
+  }
 `;
 
 export const CausasTitulo = styled.div`
-    width: 85%;
-    display: flex;
-    margin-top: 30px;
-    
+  width: 85%;
+  display: flex;
+  margin-top: 30px;
 `;
 
 export const Listagem = styled.div`
-    width: 87%;
-    min-height: 1300px;
-    display: flex;
-    justify-content: space-between; 
-    padding-left: 10px;
-    padding-right: 10px;
-    margin-top: 30px;
+  width: 100%;
+  min-height: 1300px;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 10px;
+  padding-right: 10px;
+  margin-top: 30px;
 
-    .titulos{
-        width: 60%;
-        height: 3.5%;
-    }
+  .titulos {
+    width: 60%;
+    height: 3.5%;
+  }
 
-    ul{
-        list-style-type: none;
-    }
+  ul {
+    list-style-type: none;
+  }
 `;
 
 export const CasosONG = styled.div`
-    width: 48%;
-    height: 1300px;
-    display: flex;
-    flex-direction: column;
+  width: 48%;
+  height: 1300px;
+  display: flex;
+  flex-direction: column;
+  z-index: -1;
 `;
 export const CasosAnimal = styled.div`
-    width: 48%;
-    height: 1300px;
-    display: flex;
-    flex-direction: column;
+  width: 48%;
+  height: 1300px;
+  display: flex;
+  flex-direction: column;
 `;
 
 export const CardAnimal = styled.div`
-    width: 100%;
-    height: 250px;
+  width: 100%;
+  height: 250px;
+  display: flex;
+  justify-content: space-between;
+  padding: 10px;
+  margin-top: 10px;
+  background-color: #ebebec;
+  border-radius: 8px;
+  box-shadow: 0px 5px 5px rgba(0, 0, 0, 0.2);
+
+  .media {
+    width: 42%;
+    height: 100%;
     display: flex;
-    justify-content: space-between;
-    padding: 10px;
-    margin-top: 10px;
-    background-color: #EBEBEC;  
-    border-radius: 8px;  
-    box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.3);
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
 
-    
-
-    .media{
-        width: 42%;
-        height: 100%;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-
-        .imagem{
-            width: 100%;
-            height: 77%;
-        }
-        button{
-        width: 100%;
-        height: 40px;
-        margin-top: 10px;
-        background: #453C8A;
-        border-radius: 10px;
-        color: #FFFFFF;
+    .imagem {
+      width: 100%;
+      height: 77%;
     }
+    button {
+      width: 100%;
+      height: 40px;
+      margin-top: 10px;
+      background: #453c8a;
+      border-radius: 10px;
+      color: #ffffff;
     }
-    .descricao{
-        width: 55%;
-        height: 100%;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
+  }
+  .descricao {
+    width: 55%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
 
-        h3,p{
-            margin-top: 10px;
-        }
-
-        .localizacao{
-            display: flex;
-            margin-top: 10px;
-            h4{
-                margin-left: 5px;
-            }
-        }
-        
-        
+    h3,
+    p {
+      margin-top: 10px;
     }
+
+    .localizacao {
+      display: flex;
+      margin-top: 10px;
+      h4 {
+        margin-left: 5px;
+      }
+    }
+  }
 `;
 
-
 export const CardONG = styled.div`
-    width: 100%;
-    height: 250px;
+  width: 100%;
+  height: 250px;
+  display: flex;
+  justify-content: space-between;
+  padding: 10px;
+  margin-top: 10px;
+  background-color: #ebebec;
+  border-radius: 8px;
+  box-shadow: 0px 5px 5px rgba(0, 0, 0, 0.2);
+
+  .media {
+    width: 42%;
+    height: 100%;
     display: flex;
-    justify-content: space-between;
-    padding: 10px;
-    margin-top: 10px;
-    background-color: #EBEBEC;  
-    border-radius: 8px;  
-    box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.3);
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
 
-    .media{
-        width: 42%;
-        height: 100%;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-
-        .imagem{
-            width: 100%;
-            height: 77%;
-        }
-        button{
-        width: 100%;
-        height: 40px;
-        margin-top: 10px;
-        background: #453C8A;
-        border-radius: 10px;
-        color: #FFFFFF;
+    .imagem {
+      width: 100%;
+      height: 77%;
     }
+    button {
+      width: 100%;
+      height: 40px;
+      margin-top: 10px;
+      background: #453c8a;
+      border-radius: 10px;
+      color: #ffffff;
     }
-    .descricao{
-        width: 55%;
-        height: 100%;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
+  }
+  .descricao {
+    width: 55%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
 
-        h3,p{
-            margin-top: 10px;
-        }
-
-        .localizacao{
-            display: flex;
-            margin-top: 10px;
-            h4{
-                margin-left: 5px;
-            }
-        }
-        
-        
+    h3,
+    p {
+      margin-top: 10px;
     }
 
+    .localizacao {
+      display: flex;
+      margin-top: 10px;
+      h4 {
+        margin-left: 5px;
+      }
+    }
+  }
 `;
